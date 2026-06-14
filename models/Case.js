01fb6c9925e9d85e0b6bbc107aba877083be1043
Kishan <singh000kishan@gmail.com>
2026-06-14 14:22:59 +0530

    adding relation with user model

--- models/Case.js
+++ models/Case.js
@@ -1,6 +1,14 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 const Case=require('./Case.json');
 const CaseSchema = new mongoose.Schema(Case,{
     "timestamps": true
   });
+module.exports = mongoose.model('Case', CaseSchema);*/
+
+const mongoose = require('mongoose');
+const rawSchema = require('./Case.json'); // Import the JSON above
+const {formatSchema}=require("../utility/utility.js")
+
+const  CaseSchema= new mongoose.Schema(formatSchema(rawSchema), { timestamps: true });
+
 module.exports = mongoose.model('Case', CaseSchema);
