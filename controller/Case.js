1114c5499bf5354e78f70a20af4ae0d53df90977
Kishan <singh000kishan@gmail.com>
2026-06-14 18:06:22 +0530

    GET all task and update Status

--- controller/Case.js
+++ controller/Case.js
@@ -6,7 +6,10 @@ const User =require('../models/User');
  * @param {Object} caseData - The body data sent by the user
  * @returns {Promise<Object>} The saved database document
  */
+
+//Now Manager can directly create case and assign to user
 const saveCaseToDb = async (caseData,next) => {
+  /*
   console.log(caseData)
  
   const assigne=caseData.assigne;
@@ -18,7 +21,7 @@ const saveCaseToDb = async (caseData,next) => {
     return next(error);
   }
   caseData.assignedTo=foundUser._id;
-
+*/
   const newCase = new Case(caseData);
   
   // Save it to the database
@@ -27,4 +30,79 @@ const saveCaseToDb = async (caseData,next) => {
   return savedCase;
 };
 
-module.exports = { saveCaseToDb };
+//or create the case first then assign to some user 
+const updateCaseAsigneToDb = async (caseId,updateData,next) => {
+  //console.log(caseData)
+ 
+  const assigne=updateData.assigne;
+  updateData.status="Assigned";
+  const foundUser = await User.findOne({ userid: assigne });
+
+   if (!foundUser) {
+    const error = new Error(`User with ID '${assigne}' not found`);
+    error.name = "BadRequestError"; // Triggers 400 Bad Request in your custom middleware
+    return next(error);
+  }
+  updateData.assignedTo=foundUser._id;
+
+  const updatedCase = await Case.findByIdAndUpdate(
+    caseId,
+    { $set: updateData },
+    { new: true, runValidators: true }
+  );
+
+  // 3. Handle case where the document ID does not exist
+  if (!updatedCase) {
+    const error = new Error(`Case with ID '${caseId}' not found`);
+    error.name = "BadRequestError";
+    next(error);
+    return null;
+  }
+
+  return updatedCase;
+};
+
+
+//get all task assigned to user
+const getCaseAsigneTome = async (
+assigne,next) => {
+  //console.log(caseData)
+ 
+   const cases = await Case.find({ 
+assigne:
+assigne});
+
+   if (!cases) {
+    const error = new Error(`Nothing is assign to  user with ID '${
+assigne}' `);
+    error.name = "BadRequestError"; // Triggers 400 Bad Request in your custom middleware
+    return next(error);
+  }
+
+
+  return cases;
+};
+
+//update case status
+const updateCaseStatus = async (caseId,updateData,next) => {
+  //console.log(caseData)
+ 
+    //let status=updateData.status;
+    const updatedCase = await Case.findByIdAndUpdate(
+    caseId,
+    { $set: updateData },
+    { new: true, runValidators: true }
+  );
+
+  // 3. Handle case where the document ID does not exist
+  if (!updatedCase) {
+    const error = new Error(`Case with ID '${caseId}' not found`);
+    error.name = "BadRequestError";
+    next(error);
+    return null;
+  }
+
+  return updatedCase;
+};
+
+module.exports = { saveCaseToDb,updateCaseAsigneToDb,getCaseAsigneTome,updateCaseStatus};
--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {saveCaseToDb}=require("../controller/Case.js");
+const {saveCaseToDb,updateCaseAsigneToDb,getCaseAsigneTome,updateCaseStatus}=require("../controller/Case.js");
 const {verifyRole}=require("../middleware/authMiddleware.js");
 
 // @desc    Create a new case
 // @route   POST /case
 // @access  Public
 // 1. Make this callback function async
+//for creating task i dont need asignee 
 router.post('/', verifyRole("Manager"), async (req, res, next) => {
   try {
     console.log(req.user);
@@ -23,6 +24,56 @@ router.post('/', verifyRole("Manager"), async (req, res, next) => {
   }
 });
 
+//user assigning case to Agent
+router.patch('/:caseId',verifyRole("Manager"), async (req, res, next) => {
+  try {
+    let caseId=req.params.caseId;
+    let savedCase = await updateCaseAsigneToDb(caseId,req.body, next);
+    
+    // 3. Only send this response if a case was successfully saved
+    if (savedCase) {
+      return res.status(200).json(savedCase);
+    }
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//show all case assigne to particular agent
+router.get('/', async (req, res, next) => {
+  try {
+    
+    //fetching based on user id
+    const assignedTo=req.user?.userid;
+    let totalCases = await getCaseAsigneTome(assignedTo,next);
+    
+    // 3. Only send this response if a case was successfully saved
+    if (totalCases) {
+      return res.status(200).json(totalCases);
+    }
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.patch('/', async (req, res, next) => {
+  try {
+    let caseId=req.body.caseId;
+    if(!caseId){
+          const error = new Error(`${caseId} is Mandatory field`);
+    error.name = "BadRequestError"; // Triggers 400 Bad Request in your custom middleware
+    return next(error);
+    }
+    let savedCase = await updateCaseStatus(caseId,req.body, next);
+    
+    // 3. Only send this response if a case was successfully saved
+    if (savedCase) {
+      return res.status(200).json(savedCase);
+    }
+  } catch (error) {
+    return next(error);
+  }
+});
 
 
 module.exports = router;
